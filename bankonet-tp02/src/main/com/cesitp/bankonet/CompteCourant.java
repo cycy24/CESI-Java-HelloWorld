fc2900f3ddb1b018a4c3fa4eb22a5a8dfb40fe6e
cblondy <c.blondy@outlook.fr>
2020-05-06 17:55:26 +0200

    TDD pour crediter, debiter et le total

--- bankonet-tp02/src/main/com/cesitp/bankonet/CompteCourant.java
+++ bankonet-tp02/src/main/com/cesitp/bankonet/CompteCourant.java
@@ -1,4 +1,60 @@
 package com.cesitp.bankonet;
 
 public class CompteCourant {
+    String numuero ;
+    String intitule ;
+    double solde ;
+    double montantDecouvertAutorise ;
+    static int nbComptesCourant ;
+
+
+    public CompteCourant(String numuero, String intitule, double solde, double montantDecouvertAutorise) {
+        this.numuero = numuero;
+        this.intitule = intitule;
+        this.solde = solde;
+        this.montantDecouvertAutorise = montantDecouvertAutorise;
+        CompteCourant.nbComptesCourant++;
+    }
+
+    public String getNumuero() {
+        return numuero;
+    }
+
+    public void setNumuero(String numuero) {
+        this.numuero = numuero;
+    }
+
+    public String getIntitule() {
+        return intitule;
+    }
+
+    public void setIntitule(String intitule) {
+        this.intitule = intitule;
+    }
+
+    public double getSolde() {
+        return solde;
+    }
+
+    public void setSolde(double solde) {
+        this.solde = solde;
+    }
+
+    public double getMontantDecouvertAutorise() {
+        return montantDecouvertAutorise;
+    }
+
+    public void setMontantDecouvertAutorise(double montantDecouvertAutorise) {
+        this.montantDecouvertAutorise = montantDecouvertAutorise;
+    }
+
+    public void crediter(double amount){
+        this.solde = this.solde+amount;
+
+    }
+
+    public void debiter(double amount){
+        this.solde = this.solde-amount;
+
+    }
 }
--- bankonet-tp02/src/test/com/cesitp/bankonet/CompteCourantTest.java
+++ bankonet-tp02/src/test/com/cesitp/bankonet/CompteCourantTest.java
@@ -2,6 +2,7 @@ package com.cesitp.bankonet;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,9 +10,51 @@ class CompteCourantTest {
 
     @BeforeEach
     void setUp() {
+
+
+    }
+
+    @Test
+    void totalCompte(){
+
+        CompteCourant compteCourant1 = new CompteCourant("1","Compte Courant 1",2100.0,300.0);
+
+        CompteCourant compteCourant2 = new CompteCourant("1","Compte Courant 1",2100.0,300.0);
+
+        CompteCourant compteCourant3 = new CompteCourant("1","Compte Courant 1",2100.0,300.0);
+
+        assertTrue(CompteCourant.nbComptesCourant == 3);
+
+    }
+
+    @Test
+    void crediterCompte(){
+
+        double oldSolde;
+        double addAmount = 20.0;
+        CompteCourant compteCourant1 = new CompteCourant("1","Compte Courant 1",2100.0,300.0);
+        oldSolde = compteCourant1.getSolde();
+        compteCourant1.crediter(addAmount);
+
+        assertTrue(compteCourant1.getSolde() == oldSolde+addAmount);
+
+    }
+
+    @Test
+    void debiterCompte(){
+
+        double oldSolde;
+        double remAmount = 20.0;
+        CompteCourant compteCourant1 = new CompteCourant("1","Compte Courant 1",2100.0,300.0);
+        oldSolde = compteCourant1.getSolde();
+        compteCourant1.debiter(remAmount);
+
+        assertTrue(compteCourant1.getSolde() == oldSolde-remAmount);
+
     }
 
     @AfterEach
     void tearDown() {
+        CompteCourant.nbComptesCourant = 0;
     }
 }
