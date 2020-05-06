80fc1a8ca3fba1e5536bd3d62c4f93835a02bbc2
cblondy <c.blondy@outlook.fr>
2020-05-06 14:07:40 +0200

    Version séparée

--- HelloWorldPerso.java
+++ HelloWorldPerso.java
@@ -1,7 +1,11 @@
-public class HelloWorld {
+public class HelloWorldPerso {
   // Définition de la fonction main
   public static void main(String[] args){
     // Affichage du commentaire Hello world dans la fenêtre de commande
-    System.out.println("Hello World");
+	if(args.length>0){
+		System.out.println("Hello "+args[0]);
+	}else{
+		System.out.println("Hello World");
+	}
   }
 }
