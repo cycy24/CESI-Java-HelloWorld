a15d96f8fcc24671b1e22998c9e3e5e2d368c87c
cblondy <c.blondy@outlook.fr>
2020-05-06 12:10:25 +0200

    Custom name HelloWorld

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
   // Définition de la fonction main
-  public static void main(String[] arg){
+  public static void main(String[] args){
     // Affichage du commentaire Hello world dans la fenêtre de commande
-    System.out.println("Hello world");
+    System.out.println("Hello "+args[0]);
   }
 }
